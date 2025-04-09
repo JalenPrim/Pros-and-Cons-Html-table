014a7ede0e450b70707b34ff0c73ba7ef77f990f
JalenPrim <1265087@essextech.org>
2025-04-09 13:19:45 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,11 @@
-const TheProsDiv = document.getElementById("ThePros");
-const TheConsDiv = document.getElementById("TheCons");
-const TheinfoDiv = document.getElementById("
+const TheProsDiv = document.getElementById('ThePros');
+const TheConsDiv = document.getElementById('TheCons');
+const TheInfoDiv = document.getElementById('info');
 
+// Add a click event listener to the "ThePros div
+TheProsDiv.addEventListener('click', function() {
+  TheInfoDiv.innerHTMl = 'What are some pros';
+});
+TheConsDiv.addEventListener('click', function() {
+  TheInfoDiv.innerHTML = 'what are some cons';
+});
