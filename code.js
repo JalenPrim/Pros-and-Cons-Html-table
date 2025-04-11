65fea12f0340364fe1a933dc1326251f9c3ca35c
JalenPrim <1265087@essextech.org>
2025-04-11 12:48:53 -0400

    Update code.js

--- code.js
+++ code.js
@@ -27,7 +27,7 @@ document.getElementById("checkButton").addEventListener("click", function() {
     resultText.innerText = "You can drive but can't rent";
     resultText.style.color = "blue"; // change color
     resultImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.newsweek.com%2F2024-autos-awards%2Fbest-new-vehicle-your-lifestyle-first-time-new-car-buyers-1933985&psig=AOvVaw1H8rSJrC5wgrEcKBdFCTkJ&ust=1744474434282000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwjCg4npr9CMAxW2HWIAHYWFBuIQjRx6BAgAEBs";
-  } else  (age => 25) {
+  } else  
     resultText.innerText = "You can drive and rent";
     resultText.style.color = "green"; // change color
     resultImage.src = "https%3A%2F%2Fwww.carmax.com%2Fcars%2Fhonda%3Flocation%3Dsierra%2Bvista%2Baz&psig=AOvVaw1chC1OHGqrSY7VZtbZvyI8&ust=1744391292039000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCKjrnY36zYwDFQAAAAAdAAAAABAh";
