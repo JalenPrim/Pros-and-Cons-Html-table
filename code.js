8c6f0113ae4454ee795effbf75ed5a920dfcd4de
JalenPrim <1265087@essextech.org>
2025-04-11 12:34:24 -0400

    Update code.js

--- code.js
+++ code.js
@@ -19,15 +19,15 @@ document.getElementById("checkButton").addEventListener("click", function() {
   let resultText = document.getElementById("resultText");
   let resultImage = document.getElementById("resultImage");
 
-  if (age ≤ 15 ) then {
+  if (age <= 15 ) then {
     resultText.innerText = "You cannot drive";
     resultText.style.color = "red"; // change color
     resultImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bicycling.com%2Ftraining%2Fa20048427%2Fbenefits-of-bike-riding%2F&psig=AOvVaw2CSP1sV_cBDoGCoS7XgENE&ust=1744391186512000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCNDt4fD5zYwDFQAAAAAdAAAAABA5";
-  } else if (age 17 && age 24 ) {
+  } else if (age >= 16 && age  <= 24 ) {
     resultText.innerText = "You can drive but can't rent";
     resultText.style.color = "blue"; // change color
     resultImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.newsweek.com%2F2024-autos-awards%2Fbest-new-vehicle-your-lifestyle-first-time-new-car-buyers-1933985&psig=AOvVaw1H8rSJrC5wgrEcKBdFCTkJ&ust=1744474434282000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwjCg4npr9CMAxW2HWIAHYWFBuIQjRx6BAgAEBs";
-  } else if (age 25) {
+  } else  {
     resultText.innerText = "You can drive and rent";
     resultText.style.color = "green"; // change color
     resultImage.src = "https%3A%2F%2Fwww.carmax.com%2Fcars%2Fhonda%3Flocation%3Dsierra%2Bvista%2Baz&psig=AOvVaw1chC1OHGqrSY7VZtbZvyI8&ust=1744391292039000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCKjrnY36zYwDFQAAAAAdAAAAABAh";
