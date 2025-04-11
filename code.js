6991e734d63b48f58edc8e2e30e7be6d99b013d9
JalenPrim <1265087@essextech.org>
2025-04-11 12:51:09 -0400

    Update code.js

--- code.js
+++ code.js
@@ -22,14 +22,14 @@ document.getElementById("checkButton").addEventListener("click", function() {
   if (age <= 15 ) {
     resultText.innerText = "You cannot drive";
     resultText.style.color = "red"; // change color
-    resultImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bicycling.com%2Ftraining%2Fa20048427%2Fbenefits-of-bike-riding%2F&psig=AOvVaw2CSP1sV_cBDoGCoS7XgENE&ust=1744391186512000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCNDt4fD5zYwDFQAAAAAdAAAAABA5";
+    resultImage.src = "https://i5.walmartimages.com/seo/26-Huffy-Rock-Creek-Mountain-Bike-18-Speeds-13-Years-or-Older-Blue_24d665f8-d419-47ac-bd7e-fa6549db11f8.d1c9bf17152098f64db4d14979790853.jpeg";
   } else if (age >= 16 && age  <= 24 ) {
     resultText.innerText = "You can drive but can't rent";
     resultText.style.color = "blue"; // change color
-    resultImage.src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.newsweek.com%2F2024-autos-awards%2Fbest-new-vehicle-your-lifestyle-first-time-new-car-buyers-1933985&psig=AOvVaw1H8rSJrC5wgrEcKBdFCTkJ&ust=1744474434282000&source=images&cd=vfe&opi=89978449&ved=2ahUKEwjCg4npr9CMAxW2HWIAHYWFBuIQjRx6BAgAEBs";
+    resultImage.src = "https://listings-prod.tcimg.net/listings/9912/86/32/1G1ZD5ST2NF103286/001.jpg";
   } else  
     resultText.innerText = "You can drive and rent";
     resultText.style.color = "green"; // change color
-    resultImage.src = "https%3A%2F%2Fwww.carmax.com%2Fcars%2Fhonda%3Flocation%3Dsierra%2Bvista%2Baz&psig=AOvVaw1chC1OHGqrSY7VZtbZvyI8&ust=1744391292039000&source=images&cd=vfe&opi=89978449&ved=0CBYQjRxqFwoTCKjrnY36zYwDFQAAAAAdAAAAABAh";
+    resultImage.src = "https://1000logos.net/wp-content/uploads/2021/08/Enterprise-Rent-A-Car-Logo.png";
   }
 });
